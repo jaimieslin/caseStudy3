f4e21439f189b647c537da9e640370efb64b5128
jaimieslin <48027533+jaimieslin@users.noreply.github.com>
2020-12-06 17:13:29 -0600

    Add files via upload

--- simpleRC.m
+++ simpleRC.m
@@ -1,38 +1,43 @@
-
 %% Setup and compute model
 % constants
 R = 1000; %resitance
 C = 1*10^-6; %capacitance
-range = 5.5*10^-3;
+length = 5.5*10^-3;
 
 %interval of discrete time steps
 h = 3*10^-4; 
 h_ = 8*10^-4; 
 
-steps = round(range/h);
-steps_ = round(range/h_);
+steps = fix(length/h);
+steps_ = fix(length/h_);
 
 Vin = ones(1, steps);
-Vin_ = ones(1, steps_);
 
-% calculate prediction with time steps of h
-Vc = getVoltage(R, C, h, Vin);
+% calculate perdiction with time steps of h
+Vc = zeros(1, steps);
+for i = [1:(steps-1)]
+    Vc(i+1) = (1-h/(R*C))*Vc(i) + (h/(R*C))*Vin(i);
+end
 
-
-% calculate prediction with time steps of h_
-Vc_ = getVoltage(R, C, h_, Vin_);
+% calculate perdiction with time steps of h_
+Vc_ = zeros(1, steps_);
+for i =[1:(steps_-1)]
+    Vc_(i+1) = (1-h_/(R*C))*Vc_(i) + (h_/(R*C))*Vin(i);
+end
 
 %% Plot model
 figure();
 hold on;
 %plot the input voltage
-plot((0:steps-1)*h, Vin,  'linewidth', 2); 
+plot([0:steps-1]*h, Vin,  'linewidth', 2); 
 %plot the voltage across the capacitor with time steps of h
-plot((0:steps-1)*h, Vc, 'linewidth', 2); 
+plot([0:steps-1]*h, Vc, 'linewidth', 2); 
 %plot the voltage across the capacitor with time steps of h'
-plot((0:steps_-1)*h_, Vc_, 'linewidth', 2); 
+plot([0:steps_-1]*h_, Vc_, 'linewidth', 2); 
 %plot the voltage across capacitor with the continous function
-fplot(@(t) (1-exp(-t/(R*C))),[0 range]); 
+sym t;
+V_out(t) = (1-exp(-t/(R*C)));
+fplot(@(t) V_out(t), [0 length], 'linewidth', 2); 
 
 set(gca, 'linewidth', 2);
 set(gca, 'fontsize', 14);
