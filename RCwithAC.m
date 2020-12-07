66436ea78dca6203171c63469790ab18f2a2381c
jaimieslin <48027533+jaimieslin@users.noreply.github.com>
2020-12-07 16:57:06 -0600

    Add files via upload

--- RCwithAC.m
+++ RCwithAC.m
@@ -1,5 +1,5 @@
 
-%% Setup and compute model
+%% Setup, compute, and plot model for varying frequencies
 close all;
 
 % constants
@@ -8,20 +8,29 @@ C = 1*10^-6; %capacitance
 h = 2.61*10^-6; % sampling interval
 
 figure('position', [0, 0, 1000, 1000]);
-for i = 1:4 %frequency in hertz (possible range: 10 Hz - 10,000 Hz)
-f = 10.^i;
+
+%frequency in hertz (possible range: 10 Hz - 10,000 Hz)
+freq = [10 100 1000 10000];
+
+%calculate and plot model for all frequencies in freq
+for i = 1:length(freq) 
+% define model constants
+f = freq(i);
 period = 2*1/f;
 t = 0:h:(period-h);
-steps = fix(period/h); % number of time points of interest
+steps = fix(period/h);
 
+% define and calculate model functions
 Vin = sin(2*pi*f*t);
 Vc_out = getVoltage(R, C, h, Vin);
 Vr_out = Vin - Vc_out;
 
+% play model
 playSound(Vin, (1/h));
 playSound(Vc_out, (1/h));
 playSound(Vr_out, (1/h));
 
+% plot model
 subplot(2, 2, i);
 hold on;
 plot((0:steps-1)*h, Vin,  'linewidth', 2); 
@@ -36,12 +45,13 @@ legend("V_{in}", "VC_{out}", "VR_{out}");
 title(['Voltage over time for ' num2str(f) ' Hz input']);
 hold off;
 end
-%% 
+%% Setup, compute, and plot transfer functions H(f)
 freq = 10:10:10000; % vector of 1000 frequency values spanning (10 - 10k) hz
 
 Hc = zeros(1, length(freq));
 Hr = zeros(1, length(freq));
 
+% calculate Hc and Hr
 for i = 1:length(freq)
     period = 2*1/(freq(i));
     t = 0:h:20*(period-h);
@@ -53,7 +63,7 @@ for i = 1:length(freq)
     Hr(i) = max(vr_out)/max(vin);
 end
 
-%% Plot the transfer functions H(f)
+% plot the transfer functions H(f)
 figure();
 hold on;
 
@@ -69,6 +79,7 @@ title ("Transfer function");
 
 hold off;
 
+%% Definition of function getVoltage 
 function V = getVoltage(R, C, h, Vin)
 
 steps = length(Vin);
